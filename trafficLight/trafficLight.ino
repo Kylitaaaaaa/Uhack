f7d3b4a6764d94d06491cdd7267c651abf1d230f
Kyle-Althea <kyle-althea_santos@dlsu.edu.ph>
2018-06-29 22:16:21 +0800

    can identify when to turn on green (max count)

--- trafficLight/trafficLight.ino
+++ trafficLight/trafficLight.ino
@@ -1,55 +1,61 @@
 //LightSlots
-int gL{2, 4, 6, 8};
-int rL{3, 5, 7, 9};
+// int lights[8] = {s11, s12, s21, s22, s31, s32, s41, s42};
+int lightsArr[8] = {1, 2, 3, 4, 5, 6, 7, 8};
 
 //Car Count
-int sensor1{0, 0, 0, 0};
-int sensor2{0, 0, 0, 0};
+int sensor1[4] = {0, 0, 0, 0};
+int sensor2[4] = {0, 0, 0, 0};
 
 void setup() {
   // put your setup code here, to run once:
-  
-  pinMode(gL1[0], OUTPUT);
-  pinMode(rL1[0], OUTPUT);
 
-  pinMode(gL2[1], OUTPUT);
-  pinMode(rL2[1], OUTPUT);
-
-  pinMode(gL3[2], OUTPUT);
-  pinMode(rL3[2], OUTPUT);
-
-  pinMode(gL4[3], OUTPUT);
-  pinMode(rL4[3], OUTPUT);
-
+  for(int i = 1; i< sizeof(lightsArr); i++){
+    pinMode(lightsArr[i], OUTPUT);
+  }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   getCount();
+  turnOnGreen();
 }
 
 void getCount(){
-  
+  //janz insert code here
   
 }
 
-int turnOnGreen(){
-  int temp = {0, 0, 0, 0};
+int getMax(int arr[]){
+  int max = 0;
+  for(int i = 1; i< sizeof(arr); i++){
+    if(arr[max] < arr[i])
+      max = i;
+  }
+  return max;
+}
+
+void turnOnLight(int num){
+  digitalWrite(lightsArr[num], HIGH);
+  delay(1000);
+}
+
+void turnOffLight(int num){
+  digitalWrite(lightsArr[num], LOW);
+  delay(1000);
+}
+
+void turnOnGreen(){
+  int temp [4]= {0, 0, 0, 0};
 
   //get carCount
   for(int i = 0; i < sizeof(temp); i++)
     temp[i] = sensor1[i] - sensor2[i];
 
+  //per Light
+  turnOnLight(getMax(temp));
   
 }
 
-void turnOnLight(lNum){
-  digitalWrite(lNum, HIGH);
-  delay(1000);
-}
 
-void turnOffLight(lNum){
-  digitalWrite(lNum, LOW);
-  delay(1000);
-}
+
 
