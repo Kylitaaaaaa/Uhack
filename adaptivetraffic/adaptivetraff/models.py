668a64580d02b621e509a025f5df4201559a6d6f
jasonsy1 <jason_sy@dlsu.edu.ph>
2018-06-30 00:51:59 +0800

    django-database

--- adaptivetraffic/adaptivetraff/models.py
+++ adaptivetraffic/adaptivetraff/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class stoplight(models.Model):
+    stoplightID = models.CharField(max_length=20)
+    location = models.CharField(max_length=1000)
+    #status = models.CharField(max_length=20)
+    #statusChangeTime = models.DateTimeField()
+    trafficLoad = models.IntegerField()
+    colorPhase = models.CharField(max_length=50)
+
+class violation(models.Model):
+    violationID = models.CharField(max_length=20)
+    violationType = models.CharField(max_length=1000)
+    plateNumber = models.CharField(max_length=20)
+    sensorID = models.CharField(max_length=20)
+
+class sensor(models.Model):
+    sensorID = models.CharField(max_length=20)
+    stoplightID = models.CharField(max_length=20)
+    sensorType = models.CharField(max_length=100)
--- adaptivetraffic/adaptivetraffic/settings.py
+++ adaptivetraffic/adaptivetraffic/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'adaptivetraff.apps.AdaptivetraffConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
